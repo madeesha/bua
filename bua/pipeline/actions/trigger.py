69a2e929b77212743f8f14c4f4254fe4e1dbb46b
egbev <egbev@alintaenergy.com.au>
2023-08-30 13:50:34 +1000

    Calculate run_date and today when triggering a restore if they are not provided in the trigger message

--- bua/pipeline/actions/trigger.py
+++ bua/pipeline/actions/trigger.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
-
+from datetime import datetime, timezone, timedelta
+from zoneinfo import ZoneInfo
 import yaml
 
 from bua.pipeline.facade.sqs import SQS
@@ -24,6 +25,12 @@ class Trigger:
             for key in {'suffix', 'update_id', 'snapshot_arn', 'run_date', 'today'}:
                 if key in data:
                     pipeline['data'][key] = data[key]
+            run_date = datetime.now(ZoneInfo('Australia/Sydney'))
+            if 'run_date' not in data:
+                pipeline['data']['run_date'] = run_date.strftime('%Y-%m-%d')
+            if 'today' not in data:
+                today = run_date - timedelta(days=run_date.day-1)
+                pipeline['data']['today'] = today.strftime('%Y-%m-%d')
             msg = yaml.dump(pipeline, Dumper=yaml.Dumper)
         self.sqs.send_message(self.next_queue_url, msg, delay=60)
         return "COMPLETE", f"Triggered restore"
