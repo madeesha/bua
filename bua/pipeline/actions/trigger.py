219c56ca44ab70e031501cb3e683eeaf7c7fae4e
egbev <egbev@alintaenergy.com.au>
2023-09-20 11:00:49 +1000

    Flexible template names when triggering

--- bua/pipeline/actions/trigger.py
+++ bua/pipeline/actions/trigger.py
@@ -1,7 +1,5 @@
 import os
 import pathlib
-from datetime import datetime, timezone, timedelta
-from zoneinfo import ZoneInfo
 import yaml
 
 from bua.pipeline.facade.sqs import SQS
@@ -13,7 +11,8 @@ class Trigger:
         self.next_queue_url = config['next_queue_url']
 
     def trigger_restore(self, step, data):
-        template_path = pathlib.Path(__file__).parent / 'restore_database.yml'
+        template_name = data.get('template_name', 'bua_restore')
+        template_path = pathlib.Path(__file__).parent / f'{template_name}.yml'
         if not os.path.exists(template_path):
             msg = f'Cannot find template {template_path}'
             return "ABORT", msg
@@ -25,12 +24,6 @@ class Trigger:
             for key in {'suffix', 'update_id', 'snapshot_arn', 'run_date', 'today'}:
                 if key in data:
                     pipeline['data'][key] = data[key]
-            run_date = datetime.now(ZoneInfo('Australia/Sydney'))
-            if 'run_date' not in data:
-                pipeline['data']['run_date'] = run_date.strftime('%Y-%m-%d')
-            if 'today' not in data:
-                today = run_date - timedelta(days=run_date.day-1)
-                pipeline['data']['today'] = today.strftime('%Y-%m-%d')
             msg = yaml.dump(pipeline, Dumper=yaml.Dumper)
         self.sqs.send_message(self.next_queue_url, msg, delay=60)
         return "COMPLETE", f"Triggered restore"
