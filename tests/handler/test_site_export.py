b7bb551e7db2bda5f1fc161c4aeb1c59c125777a
egbev <egbev@alintaenergy.com.au>
2023-10-04 16:36:00 +1100

    test reconnect failure on site_data, site_export, site_initiate, site_mscalar, site_nem12, site_segment

--- tests/handler/test_site_export.py
+++ tests/handler/test_site_export.py
@@ -34,3 +34,22 @@ class TestCase:
             }
             context = {}
             lambda_handler(event, context)
+
+    def test_invoke_handler_reconnect_failure(self):
+        import tests.handler.monkey_patch as monkey_patch
+        monkey_patch.patch.patch(environ=self._environ)
+        with pytest.raises(RuntimeError) as ex:
+            from bua.handler.site_export import lambda_handler, handler
+            handler.log = monkey_patch.patch.log
+            monkey_patch.patch.connect().cursor().execute_fails_after_invocations = 0
+            event = {
+                'Records': [
+                    {
+                        'eventSource': 'aws:sqs'
+                    }
+                ]
+            }
+            context = {}
+            lambda_handler(event, context)
+        assert str(ex.value).startswith('Failed to handle request')
+        monkey_patch.patch.assert_log('Failed to reconnect to the database after a failure')
