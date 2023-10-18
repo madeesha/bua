62f0ab2e08d1d0c169dda91d71884566c5db8cc2
egbev <egbev@alintaenergy.com.au>
2023-10-18 16:05:09 +1100

    Refactor wait_for_empty_site_queues to be more readable

--- bua/pipeline/actions/profiles.py
+++ bua/pipeline/actions/profiles.py
@@ -14,6 +14,31 @@ class Profiles:
         status = 'COMPLETE'
         reason = 'None of the queues have messages'
         queues = self.sqs.describe_queues(queue_name_prefix=f'{self.prefix}-sqs-bua-site-')
+        status, reason = self._check_for_messages_to_process(queues, status, reason)
+        if status == 'COMPLETE':
+            status, reason = self._check_for_message_failures(queues, status, reason)
+        return status, reason
+
+    def _check_for_message_failures(self, queues, status, reason):
+        for queue_url, attributes in queues.items():
+            queue_name = queue_url.split('/')[-1]
+            total = attributes['Total']
+            if total > 0:
+                if queue_name.endswith('-failure-queue'):
+                    status = 'FAILED'
+                    reason = f'Queue {queue_name} has about {total} messages'
+                    break
+                if queue_name.endswith('-dlqueue'):
+                    status = 'FAILED'
+                    reason = f'Queue {queue_name} has about {total} messages'
+                    break
+                status = 'RETRY'
+                reason = f'Queue {queue_name} has about {total} messages'
+                break
+        return status, reason
+
+    @staticmethod
+    def _check_for_messages_to_process(queues, status, reason):
         for queue_url, attributes in queues.items():
             queue_name = queue_url.split('/')[-1]
             if queue_name.endswith('-failure-queue'):
@@ -25,22 +50,6 @@ class Profiles:
                 status = 'RETRY'
                 reason = f'Queue {queue_name} has about {total} messages'
                 break
-        if status == 'COMPLETE':
-            for queue_url, attributes in queues.items():
-                queue_name = queue_url.split('/')[-1]
-                total = attributes['Total']
-                if total > 0:
-                    if queue_name.endswith('-failure-queue'):
-                        status = 'FAILED'
-                        reason = f'Queue {queue_name} has about {total} messages'
-                        break
-                    if queue_name.endswith('-dlqueue'):
-                        status = 'FAILED'
-                        reason = f'Queue {queue_name} has about {total} messages'
-                        break
-                    status = 'RETRY'
-                    reason = f'Queue {queue_name} has about {total} messages'
-                    break
         return status, reason
 
     def empty_site_errors_queues(self, _request: HandlerRequest):
