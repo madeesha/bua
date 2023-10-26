92cdc4053cd99e6d2eb2689473802707f01a8bd8
egbev <egbev@alintaenergy.com.au>
2023-10-26 14:50:26 +1100

    Deal with DBSnapshotNotFound error

--- bua/facade/rds.py
+++ bua/facade/rds.py
@@ -1,3 +1,8 @@
+from typing import Dict
+
+from botocore.exceptions import ClientError
+
+
 class RDS:
 
     def __init__(self, rds_client):
@@ -8,11 +13,9 @@ class RDS:
 
     def copy_snapshot(self, snapshot_arn: str, snapshot_name: str, kms_key_id: str, option_group_name: str):
         snapshot_id = f'rds:{snapshot_name}'
-        response = self.rds.describe_db_snapshots(DBSnapshotIdentifier=snapshot_id)
-        if 'DBSnapshots' in response:
-            for snapshot in response['DBSnapshots']:
-                if snapshot['DBSnapshotIdentifier'] == snapshot_id:
-                    return snapshot['DBSnapshotArn']
+        snapshot = self._describe_db_snapshot(snapshot_id=snapshot_id)
+        if 'DBSnapshotArn' in snapshot:
+            return snapshot['DBSnapshotArn']
         response = self.rds.copy_db_snapshot(
             SourceDBSnapshotIdentifier=snapshot_arn,
             TargetDBSnapshotIdentifier=snapshot_name,
@@ -23,9 +26,27 @@ class RDS:
 
     def check_snapshot_status(self, snapshot_name: str) -> str:
         snapshot_id = f'rds:{snapshot_name}'
-        response = self.rds.describe_db_snapshots(DBSnapshotIdentifier=snapshot_id)
-        if 'DBSnapshots' in response:
-            for snapshot in response['DBSnapshots']:
-                if snapshot['DBSnapshotIdentifier'] == snapshot_id:
-                    return snapshot['Status']
-        return 'Unknown'
+        snapshot = self._describe_db_snapshot(snapshot_id=snapshot_id)
+        return snapshot['Status']
+
+    def _describe_db_snapshot(self, snapshot_id: str) -> Dict:
+        try:
+            response = self.rds.describe_db_snapshots(DBSnapshotIdentifier=snapshot_id)
+            if 'DBSnapshots' in response:
+                for snapshot in response['DBSnapshots']:
+                    if snapshot['DBSnapshotIdentifier'] == snapshot_id:
+                        return snapshot
+        except ClientError as ex:
+            if 'Error' in ex.response:
+                error = ex.response['Error']
+                if 'Code' in error and error['Code'] == 'DBSnapshotNotFound':
+                    return {
+                        'Status': 'NotFound'
+                    }
+                else:
+                    raise
+            else:
+                raise
+        return {
+            'Status': 'Unknown'
+        }
