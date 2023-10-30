e50a1347eaed138d573d0adc407f46653d44302b
egbev <egbev@alintaenergy.com.au>
2023-10-30 14:40:10 +1100

    Fetch SSM parameters maximum of 10 at a time

--- bua/facade/ssm.py
+++ bua/facade/ssm.py
@@ -6,14 +6,18 @@ class SSM:
         self.ssm_client = ssm_client
 
     def get_parameters(self, names: List[str]) -> Dict[str, str]:
-        response = self.ssm_client.get_parameters(Names=names, WithDecryption=True)
-        if 'InvalidParameters' in response:
-            invalid_parameters = response['InvalidParameters']
-            if len(invalid_parameters) > 0:
-                raise ValueError(f'Invalid parameters {invalid_parameters}')
-        if 'Parameters' in response:
-            return {param['Name']: param['Value'] for param in response['Parameters']}
-        return dict()
+        param_values = dict()
+        for segment in range(0, len(names), 10):
+            call_names = names[segment: min(len(names), segment+10)]
+            response = self.ssm_client.get_parameters(Names=call_names, WithDecryption=True)
+            if 'InvalidParameters' in response:
+                invalid_parameters = response['InvalidParameters']
+                if len(invalid_parameters) > 0:
+                    raise ValueError(f'Invalid parameters {invalid_parameters}')
+            if 'Parameters' in response:
+                for param in response['Parameters']:
+                    param_values[param['Name']] = param['Value']
+        return param_values
 
     def put_parameter(self, name: str, value: str):
         self.ssm_client.put_parameter(Name=name, Value=value, Type='String', Overwrite=True, DataType='text')
